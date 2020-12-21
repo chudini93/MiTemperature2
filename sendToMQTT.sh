0ac15bec9928b19427a5fc1f723144299471d060
chudini93 <bartlomiejmarek93@gmail.com>
2020-12-21 12:10:34 +0100

    Update sendToMQTT.sh

--- sendToMQTT.sh
+++ sendToMQTT.sh
@@ -9,7 +9,10 @@
 #DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 #$DIR/LYWSD03MMC.py -d <device> -b 1000 -r --debounce --skipidentical 50 --name MySensor --callback sendToMQTT
 
-mosquitto_pub -h mqtt.host -t "MiTemperature2/$2/temp" -u mqtt.username -P mqtt.passwd -i "mibridge" -m "$3"
-mosquitto_pub -h mqtt.host -t "MiTemperature2/$2/humidity" -u mqtt.username -P mqtt.passwd -i "mibridge" -m "$4"
-mosquitto_pub -h mqtt.host -t "MiTemperature2/$2/batteryvoltage" -u mqtt.username -P mqtt.passwd -i "mibridge" -m "$5"
-mosquitto_pub -h mqtt.host -t "MiTemperature2/$2/batterylevel" -u mqtt.username -P mqtt.passwd -i "mibridge" -m "$6"
+mqtt_host="192.168.31.253"
+mqtt_topic=xiaomi-temperature2
+
+mosquitto_pub -h $mqtt_host -t "$mqtt_topic/$2/temp"           -i "mibridge" -m "$3"
+mosquitto_pub -h $mqtt_host -t "$mqtt_topic/$2/humidity"       -i "mibridge" -m "$4"
+mosquitto_pub -h $mqtt_host -t "$mqtt_topic/$2/batteryvoltage" -i "mibridge" -m "$5"
+mosquitto_pub -h $mqtt_host -t "$mqtt_topic/$2/batterylevel"   -i "mibridge" -m "$6"
